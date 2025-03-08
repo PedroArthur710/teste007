b926e6fe62a084d2e63d24ed332a3af45971bf2f
PedroArthur710 <p.arthur@iesb.edu.br>
2025-03-07 23:17:32 -0300

    atualizando o git

---
+++ 03 Revisao Java/Exercicios/page4.js
@@ -0,0 +1,11 @@
+function somar(numA, numB) {
+    return numA + numB
+  }
+  console.log(somar(2, 2))
+  console.log(somar(10, 2))
+  console.log(somar(5, 5))
+  
+  function saudacao(nome) {
+    console.log("Olá " + nome)
+  }
+  saudacao("Pedro")
